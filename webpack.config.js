79537266deccc4ec11c66359ef0bdefbfdde7944
Maarten von Kreijfelt <m.vonkreijfelt@preludio.nl>
2019-08-02 15:36:03 +0200

    message

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+module.exports = [{
+		entry: {
+		main: ['./lib/main.js'],
+		},
+		output: {
+		path: path.resolve(__dirname, 'build'),
+		publicPath: '/assets/',
+		filename: 'bundle.js',
+		},
+		devtool: 'inline-source-map',
+		module: {
+		rules: [{
+		test: /.js?$/,
+		exclude: /(node_modules|bower_components)/,
+		use: {
+                loader: "babel-loader",
+                options: {
+                    presets: ['@babel/preset-env']
+                }
+            }
+		}, {
+		test: /.json$/,
+		loader: 'json-loader',
+		}, {
+		test: /.css$/,
+		//loader: 'style-loader!css-loader',
+       use:['style-loader','css-loader'],
+
+
+
+
+		}],
+	},
+}];
